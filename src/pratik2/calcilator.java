0260d172ca33b47ae47e90339596d8373e25ae97
Gurkan Gurgenli <gurkangurgenli@gmail.com>
2024-01-21 00:53:36 +0300

    calcilator added

--- src/pratik2/calcilator.java
+++ src/pratik2/calcilator.java
@@ -1,34 +1,36 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class calcilator {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        double a, b;
+        int a, b;
         System.out.println("Yapmak istediğiniz dört işlemi belirtin: ");
         System.out.println("1-Toplama\n2-Çıkarma\n3-Çarpma\n4-Bölme");
-        double islem = input.nextDouble();
+        int islem = input.nextInt();
         System.out.println(("1'inci sayıyı giriniz: "));
-        a = input.nextDouble();
+        a = input.nextInt();
         System.out.println(("2'nci sayıyı giriniz: "));
-        b = input.nextDouble();
+        b = input.nextInt();
 
-        if (islem == 1) {
-            System.out.println("Sonuç= " + (a + b));
-        } else if (islem == 2) {
-            System.out.println("Sonuç= " + (a - b));
-        } else if (islem == 3) {
-            System.out.println("Sonuç= " + (a * b));
-        } else if (islem == 4) {
-            System.out.println("Sonuç= " + (a / b));
-            if (b == 0) {
-                System.out.println("Sıfır bölünmez = Tanımsız!");
-            }
-        } else {
-            System.out.println("HATA!");
-
-
+        switch (islem) {
+            case 1:
+                System.out.println(a+b);
+                break;
+            case 2:
+                System.out.println(a-b);
+                break;
+            case 3:
+                System.out.println(a*b);
+                break;
+            case 4:
+                if(b==0){
+                    System.out.println("Sıfıra bölüm olmaz!");
+                }
+                System.out.println(a/b);
+                break;
+        }
         }
 
 
     }
-}
