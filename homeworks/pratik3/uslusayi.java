3639f86d019d64ec6e6fe54e3cf99e4beb44120d
Gurkan Gurgenli <gurkangurgenli@gmail.com>
2024-01-24 22:08:35 +0300

    uslusayi

---
+++ homeworks/pratik3/uslusayi.java
@@ -0,0 +1,18 @@
+package pratik3;
+import java.util.Scanner;
+
+public class uslusayi {
+    public static void main(String[] args) {
+        Scanner inp =new Scanner(System.in);
+        int c, r;
+        System.out.println("Sayı Gir= ");
+        c= inp.nextInt();
+        System.out.println("Sayı Gir= ");
+        r= inp.nextInt();
+        int total=1;
+        for(int i=1; i<=r; i++){
+            total *= c;
+        }
+        System.out.println("Total= " +total);
+    }
+}
